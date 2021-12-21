902561694d93187df9b95c2a6e4fe096e2ee5545
st-lana <st.lana@hotmail.com>
2021-12-21 19:21:32 +0300

    Statistic service added

---
+++ src/test/java/ru/netology/statistics/StatisticsServiceTest.java
@@ -0,0 +1,10 @@
+package ru.netology.statistics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void findMax() {
+    }
+}
